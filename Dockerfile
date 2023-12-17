2e88b3cd072aa0e4ebc0450611b51083e46cb701
Rangga Manggala Yudha <ranggaamyd@gmail.com>
2023-12-17 22:20:39 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,12 @@
-# Gunakan node image sebagai base image
-FROM node:14
+FROM node:18
 
-# Set working directory di dalam container
 WORKDIR /usr/src/app
 
-# Copy package.json dan package-lock.json jika ada, kemudian install dependencies
 COPY package*.json ./
 RUN npm install
 
-# Copy seluruh kode aplikasi ke dalam container
 COPY . .
 
-# Port yang digunakan oleh aplikasi
 EXPOSE 8080
 
-# Command untuk menjalankan aplikasi saat container dijalankan
 CMD ["node", "src/main.js"]
--- src/application/database.js
+++ src/application/database.js
@@ -2,24 +2,24 @@ import { PrismaClient } from "@prisma/client";
 import { logger } from "./logging.js";
 
 export const prismaClient = new PrismaClient({
-  // log: [
-  //   {
-  //     emit: "event",
-  //     level: "query",
-  //   },
-  //   {
-  //     emit: "event",
-  //     level: "error",
-  //   },
-  //   {
-  //     emit: "event",
-  //     level: "info",
-  //   },
-  //   {
-  //     emit: "event",
-  //     level: "warn",
-  //   },
-  // ],
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
 });
 
 prismaClient.$on("query", (e) => {
