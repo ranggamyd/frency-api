a21e2658a1b92aefaf43d687582fefb57d0c9eb2
Rangga Manggala Yudha <ranggaamyd@gmail.com>
2023-12-18 03:12:32 +0700

    edit url

--- src/route/api.js
+++ src/route/api.js
@@ -8,24 +8,23 @@ const userRouter = new express.Router();
 userRouter.use(authMiddleware);
 
 // User API
-userRouter.post("/api/users", userController.register);
-userRouter.get("/api/users", userController.getAll);
-userRouter.get("/api/users/current", userController.get);
-userRouter.patch("/api/users/current", userController.update);
-userRouter.delete("/api/users/logout", userController.logout);
+userRouter.get("/users", userController.getAll);
+userRouter.get("/users/current", userController.get);
+userRouter.patch("/users/current", userController.update);
+userRouter.delete("/users/logout", userController.logout);
 
 // Franchise API
-userRouter.post("/api/franchises", franchiseController.create);
+userRouter.post("/franchises", franchiseController.create);
 userRouter.post(
-  "/api/franchises/:franchiseId/upload",
+  "/franchises/:franchiseId/upload",
   uploadMiddleware,
   franchiseController.uploadImages
 );
-userRouter.get("/api/franchises", franchiseController.getAll);
-userRouter.get("/api/my_franchises", franchiseController.getMyFranchises); // Posted By Franchisor
-userRouter.get("/api/franchises/:franchiseId", franchiseController.get);
-userRouter.put("/api/franchises/:franchiseId", franchiseController.update);
-userRouter.delete("/api/franchises/:franchiseId", franchiseController.remove);
-userRouter.get("/api/franchises", franchiseController.search);
+userRouter.get("/franchises", franchiseController.getAll);
+userRouter.get("/my_franchises", franchiseController.getMyFranchises); // Posted By Franchisor
+userRouter.get("/franchises/:franchiseId", franchiseController.get);
+userRouter.put("/franchises/:franchiseId", franchiseController.update);
+userRouter.delete("/franchises/:franchiseId", franchiseController.remove);
+userRouter.get("/franchises", franchiseController.search);
 
 export { userRouter };
--- src/route/public-api.js
+++ src/route/public-api.js
@@ -2,7 +2,7 @@ import express from "express";
 import userController from "../controller/user-controller.js";
 
 const publicRouter = new express.Router();
-publicRouter.post("/api/users", userController.register);
-publicRouter.post("/api/users/login", userController.login);
+publicRouter.post("/users", userController.register);
+publicRouter.post("/users/login", userController.login);
 
 export { publicRouter };
